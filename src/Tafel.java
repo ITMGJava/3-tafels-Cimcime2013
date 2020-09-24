24c1fdf43add846d990422b7789a3eb341e5b479
cimcim <sergensol@gmail.com>
2020-09-24 11:05:42 +0200

    eerste commit

--- src/Tafel.java
+++ src/Tafel.java
@@ -1,2 +1,32 @@
 public class Tafel {
+
+    public static void main(String[] args) {
+
+
+        int y =8;
+
+
+       for (int teller=1; teller<=10;teller++)
+
+
+        {
+
+            int uitkomst= y*teller;
+            System.out.println(teller + " * 8" + " = " +   uitkomst );
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
 }
